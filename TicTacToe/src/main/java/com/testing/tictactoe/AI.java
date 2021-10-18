1e1f6f774b6fe9aa5d6226a63bc60109cd14d668
Gavin Leach <gcleach05@gmail.com>
2021-10-18 09:06:57 -0700

    Implemented DeepNetts Library

---
+++ TicTacToe/src/main/java/com/testing/tictactoe/AI.java
@@ -0,0 +1,15 @@
+package com.testing.tictactoe;
+
+
+import deepnetts.net.layers.activation.ActivationType;
+import deepnetts.net.loss.LossType;
+import deepnetts.net.FeedForwardNetwork;
+
+public class AI {
+        FeedForwardNetwork nueralNet = FeedForwardNetwork.builder()
+                .addInputLayer(57)
+                .addFullyConnectedLayer(15)
+                .addOutputLayer(1, ActivationType.SIGMOID)
+                .lossFunction(LossType.CROSS_ENTROPY)
+                .build();
+}
